0dcc6d5cf3a01bdf35125ebad0dc83458ff1df5d
RUDNPRO <DbIM@mail.ru>
2020-11-02 17:50:08 +0300

    DZ0.3

---
+++ src/ru/progwards/java1/lessons/compare_if_cycles/TriangleInfo.java
@@ -0,0 +1,31 @@
+package ru.progwards.java1.lessons.compare_if_cycles;
+
+public class TriangleInfo {
+    public static void main(String[] args) {
+        System.out.println(isTriangle(3, 2, 5));
+    }
+
+    public static boolean isTriangle(int a, int b, int c){
+        if (a < b + c && b < a + c && c < a + b) {
+            return (true);
+        }
+        else
+            return (false);
+    }
+
+    public static boolean isRightTriangle(int a, int b, int c){
+        if (((a*a) == (b*b) + (c*c)) || ((b*b) == (a*a) + (c*c)) || ((c*c) == (b*b) + (a*a))) {
+            return (true);
+        }
+        else
+            return (false);
+    }
+
+    public static boolean isIsoscelesTriangle(int a, int b, int c){
+        if ((a == b) || (b == c) || (a == c)) {
+            return (true);
+        }
+        else
+            return (false);
+    }
+}
--- src/ru/progwards/java1/lessons/test/Test1.java
+++ src/ru/progwards/java1/lessons/test/Test1.java
@@ -2,6 +2,39 @@ package ru.progwards.java1.lessons.test;
 
 public class Test1 {
     public static void main(String[] args) {
-        System.out.println("Сделаю коммит, запушу в репо: робот, проверяй теперь всё это...");
+        System.out.println(factorial(19));
     }
+
+    static String textGrade(int grade) {
+        if (grade == 0) {
+            return ("не оценено");
+        } else if (grade >= 1 && grade <= 20) {
+            return ("очень плохо");
+        } else if (grade >= 21 && grade <= 40) {
+            return ("плохо");
+        } else if (grade >= 41 && grade <= 60) {
+            return ("удовлетворительно");
+        } else if (grade >= 61 && grade <= 80) {
+            return ("хорошо");
+        } else if (grade >= 81 && grade <= 100) {
+            return ("отлично");
+        } else {
+            return ("не определено");
+        }
+    }
+
+    static long factorial(long n) {
+        long i = 1;
+        if (n == 0) {
+            return (1);
+        } else {
+            while (n >= 1) {
+                i = i * n;
+                n--;
+            }
+            return i;
+        }
+    }
+
 }
+
