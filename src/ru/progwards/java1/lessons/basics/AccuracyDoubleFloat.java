03f6529b2e0383eb8dea1acac6b517c741cd43ac
RUDNPRO <DbIM@mail.ru>
2020-11-02 15:29:28 +0300

    DZ2.2

--- src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
+++ src/ru/progwards/java1/lessons/basics/AccuracyDoubleFloat.java
@@ -5,15 +5,14 @@ public class AccuracyDoubleFloat {
     public static Double pi = 3.14;
 
     public static double volumeBallDouble(double radius) {
-        return (4 / (3 * pi * (radius * radius * radius)));
+        return (4 * pi * (radius * radius * radius) / 3);
     }
 
     public static float volumeBallFloat(float radius) {
-        return (float) (4 / (3 * pi * (radius * radius * radius)));
+        return (float) (4 * pi * (radius * radius * radius) / 3);
     }
 
     public static double calculateAccuracy(double radius) {
         return (volumeBallDouble(radius) - volumeBallFloat((float) radius));
     }
-
 }
