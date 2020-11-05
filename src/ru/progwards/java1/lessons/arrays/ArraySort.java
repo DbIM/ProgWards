9a664eb4e5b7454c45b3b8cd0b6900712bbe708f
RUDNPRO <DbIM@mail.ru>
2020-11-05 17:14:49 +0300

    zaebalsa222

--- src/ru/progwards/java1/lessons/arrays/ArraySort.java
+++ src/ru/progwards/java1/lessons/arrays/ArraySort.java
@@ -1,14 +1,15 @@
 package ru.progwards.java1.lessons.arrays;
 
 public class ArraySort {
-    public static void sort(int[] a){
-        for (int i=0;i<a.length;i++){
-            for (int j = i +1; j<a.length; j++){
+    public static void sort(int[] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int j = i + 1; j < a.length; j++) {
                 int x;
-                if (a[i] < a[j]){
+                if (a[i] < a[j]) {
                     x = a[i];
                     a[i] = a[j];
                     a[j] = x;
+
                 }
             }
         }
--- src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
+++ src/ru/progwards/java1/lessons/arrays/Eratosthenes.java
@@ -16,13 +16,14 @@ public class Eratosthenes {
             for (int i = 2; i < sieve.length - 1; i++) {
                 if ((i + 1) % n == 0) {
                     sieve[i] = false;
+
                 }
             }
         }
     }
 
-    public boolean isSimple(int n){
-        if (sieve[n] == true){
+    public boolean isSimple(int n) {
+        if (sieve[n] == true) {
             return (true);
         }
         return (false);
