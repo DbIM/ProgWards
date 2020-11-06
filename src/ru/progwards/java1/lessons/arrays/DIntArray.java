40d0505cf22803d2ff7836a7f0413193f092f73f
RUDNPRO <DbIM@mail.ru>
2020-11-06 13:11:36 +0300

    Arrays added (remaster)

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -3,49 +3,46 @@ package ru.progwards.java1.lessons.arrays;
 import java.util.Arrays;
 
 public class DIntArray {
-    private int[] arr = new int[]{};
+    private int[] array = new int[]{};
+    private int[] getArray() {
+        return array;
+    }
+
 
     public void add(int num) {
-
-        if (num > arr.length) {
-            return;
-        }
-
-        int[] arrayInt = new int[]{};
-        arrayInt = Arrays.copyOf(arr, arr.length + 1);
-        arrayInt[arrayInt.length - 1] = num;
+        int[] arrayCopy = Arrays.copyOf(getArray(), getArray().length + 1); //добавляем длину
+        arrayCopy[arrayCopy.length - 1] = num;   //последний индекс= num
+        array = arrayCopy;
     }
 
     public void atInsert(int pos, int num) {
-        int tempDigital = 0;
-        int[] arrayIntIns = Arrays.copyOf(arr, arr.length + 1);
-
-
-        tempDigital = arrayIntIns[pos];
-        arrayIntIns[pos] = num;
-        arrayIntIns[arrayIntIns.length - 1] = tempDigital;
+        int[] arrayCopy = new int[array.length + 1];
+        for (int i = 0; i < arrayCopy.length; i++) {
+            if (i < pos) {
+                arrayCopy[i] = array[i];
+            } else if (i == pos) {
+                arrayCopy[i] = num;
+            } else {
+                arrayCopy[i] = array[i - 1];
+            }
+        }
+        array = arrayCopy;
     }
 
 
     public void atDelete(int pos) {
-        int j = 0;
-        int arrayIntDel[] = new int[arr.length - 1];
-
-
-        for (int i = 0; i < arr.length; i++) {
-            if (i == pos) {
-                arrayIntDel[j] = arr[i + 1];
-            } else {
-                arrayIntDel[j] = arr[i];
-                j++;
+        int[] arrayCopy = new int[getArray().length - 1];
+        for (int i = 0; i < array.length; i++) {
+            if (i < pos) {
+                arrayCopy[i] = array[i];
+            } else if (i > pos) {
+                arrayCopy[i - 1] = array[i];
             }
         }
-
+        array = arrayCopy;
     }
 
     public int at(int pos) {
-        return arr[pos];
+        return array[pos];
     }
-
-
 }
