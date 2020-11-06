a99555c18070e15e815ed864035ad3c782b08306
RUDNPRO <DbIM@mail.ru>
2020-11-06 11:13:35 +0300

    Arrays added (remaster)

--- src/ru/progwards/java1/lessons/arrays/DIntArray.java
+++ src/ru/progwards/java1/lessons/arrays/DIntArray.java
@@ -4,38 +4,23 @@ import java.util.Arrays;
 
 public class DIntArray {
 
-
     private int[] array = new int[]{};
 
-    public DIntArray(){
-    }
-
-/*    public DIntArray(int[] array) {
+    public DIntArray(int[] array) {
         this.array = array;
-    }*/
-
-    int arrayLenght = array.length + 1;
-
-    //добавляет элемент num в конец массива, при этом размер массива должен увеличиться на 1.
-    // Для этого нужно будет разместить новый массив нужного размера,
-    // скопировать в него старый, и добавить в хвост элемент num.
-    public void add(int num) {
-        int[] newArray = Arrays.copyOf(array, arrayLenght);
-        newArray[arrayLenght] = num;
     }
 
-    //добавляет элемент num в позицию pos массива, при этом размер массива должен увеличиться на 1.
-    // Для этого нужно будет разместить новый массив нужного размера, скопировать в него старый,
-    // c учетом того, что новый элемент окажется где-то в середине,
-    // и потом положить в нужный индекс элемент num.
+
+    public void add(int num) {
+        int[] newArray = Arrays.copyOf(array, array.length + 1);
+        newArray[array.length] = num;
+    }
+
     public void atInsert(int pos, int num) {
-        int[] newArray = Arrays.copyOf(array, arrayLenght);
+        int[] newArray = Arrays.copyOf(array, array.length);
         newArray[pos] = num;
     }
 
-    //удаляет элемент в позиции pos массива, при этом размер массива должен уменьшиться на 1.
-    // Для этого нужно будет разместить новый массив нужного размера,
-    // скопировать в него старый, уже без элемента, который был в позиции pos.
     public void atDelete(int pos) {
         array[pos] = 0;
         Arrays.sort(array);
