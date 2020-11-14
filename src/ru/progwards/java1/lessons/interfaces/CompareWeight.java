c690e32cd10b68a9986c8b138b9bb1df754de682
RUDNPRO <DbIM@mail.ru>
2020-11-14 17:26:29 +0300

    dz 8 1.test3

--- src/ru/progwards/java1/lessons/interfaces/CompareWeight.java
+++ src/ru/progwards/java1/lessons/interfaces/CompareWeight.java
@@ -1,6 +1,8 @@
 package ru.progwards.java1.lessons.interfaces;
 
 public interface CompareWeight {
+    public CompareResult compareWeight(CompareWeight smthHasWeigt);
+
     public enum CompareResult {
         LESS,EQUAL,GREATER
     };
