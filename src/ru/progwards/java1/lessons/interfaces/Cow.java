05823e48e9c5db5bff88436805cf217bdd253a70
RUDNPRO <DbIM@mail.ru>
2020-11-14 16:39:37 +0300

    dz 8 1.test2

---
+++ src/ru/progwards/java1/lessons/interfaces/Cow.java
@@ -0,0 +1,25 @@
+package ru.progwards.java1.lessons.interfaces;
+
+public class Cow extends Animal {
+
+    public Cow(double weight) {
+        super(weight);
+    }
+
+    @Override
+    public double getFoodCoeff() {
+        return (0.05);
+    }
+
+    @Override
+    public AnimalKind getKind() {
+        AnimalKind animal = AnimalKind.COW;
+        return animal;
+    }
+
+    @Override
+    public FoodKind getFoodKind() {
+        FoodKind food = FoodKind.HAY;
+        return food;
+    }
+}
---
+++ src/ru/progwards/java1/lessons/interfaces/Duck.java
@@ -0,0 +1,24 @@
+package ru.progwards.java1.lessons.interfaces;
+
+public class Duck extends Animal {
+    public Duck(double weight) {
+        super(weight);
+    }
+
+    @Override
+    public double getFoodCoeff() {
+        return (0.04);
+    }
+
+    @Override
+    public AnimalKind getKind() {
+        AnimalKind animal = AnimalKind.DUCK;
+        return animal;
+    }
+
+    @Override
+    public FoodKind getFoodKind() {
+        FoodKind food = FoodKind.CORN;
+        return food;
+    }
+}
