35bbd08f32ba7285c62f74621c45b30eca9c022a
RUDNPRO <DbIM@mail.ru>
2020-12-03 15:35:01 +0300

    lesson 9 DZ ArrayInteger

---
+++ src/ru/progwards/java1/lessons/bigints/ArrayInteger.java
@@ -0,0 +1,28 @@
+package ru.progwards.java1.lessons.bigints;
+
+import java.math.BigInteger;
+
+public class ArrayInteger {
+    byte[] digits;
+
+    ArrayInteger(int n){
+        digits = new byte[n];
+    }
+}
+
+
+//Реализовать класс ArrayInteger - целого числа произвольной длины на массиве byte[] digits;
+// Каждый элемент массива digits[i] может хранить только цифру, то есть число от 0 до 9.
+// Например, число 159 должно занять 3 ячейки массива digits[0] = 9; digits[1] = 5; digits[2] = 1;
+//
+//Реализовать методы:
+//3.1 ArrayInteger(int n) - инициализирует класс, с максимальной точностью n цифр (размер массива)
+//
+//3.2 void fromInt(BigInteger value) - установить свое значение, взяв его из value
+// (уложить BigInteger во внутренний массив)
+//
+//3.3 BigInteger toInt() - привести свое значение к BigInteger (преобразовать из массива в BigInteger)
+//
+//3.4 boolean add(ArrayInteger num) - сложить 2 числа, не используя BigInteger,
+// а  используя массив digits, результат поместить в экземпляр ArrayInteger,
+// у которого был вызван метод. При переполнении вернуть false, при этом само число сбросить в 0
--- src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
+++ src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
@@ -6,19 +6,19 @@ import java.math.BigInteger;
 //Реализовать алгоритм быстрого возведения в степень pow
 // числа num в BigDecimal
 public class BigAlgebra {
-    BigDecimal fastPow(BigDecimal num, int pow){
+    static BigDecimal fastPow(BigDecimal num, int pow) {
         BigDecimal k1 = num;
         BigDecimal k2;
         if (pow % 2 == 1) {
-            int pow2 = ((pow - 1)/2);
-            for (int i = 1; i<pow2; i++){
+            int pow2 = ((pow - 1) / 2);
+            for (int i = 1; i < pow2; i++) {
                 k1 = k1.multiply(num);
             }
             k2 = k1.multiply(k1).multiply(num);
             return k2;
         }
         int pow2 = pow / 2;
-        for (int i = 1; i<pow2; i++){
+        for (int i = 1; i < pow2; i++) {
             k1 = k1.multiply(num);
         }
         k2 = k1.multiply(k1);
@@ -26,11 +26,17 @@ public class BigAlgebra {
     }
 
     //Реализовать алгоритм вычисления n-го числа фибоначчи в BigInteger.
-    BigInteger fibonacci(int n){
+    static BigInteger fibonacci(int n) {
         if (n == 1 || n == 2) {
             return BigInteger.valueOf(1);
         }
-            return BigInteger.valueOf(((n - 1) + (n - 2)));
+        return BigInteger.valueOf(((n - 1) + (n - 2)));
+    }
+
+    public static void main(String[] args) {
+        BigDecimal x = new BigDecimal("2");
+        System.out.println(fastPow(x, 11));
+        System.out.println(fibonacci(25));
     }
 }
 
