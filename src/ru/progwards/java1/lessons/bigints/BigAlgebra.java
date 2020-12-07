2c34cf1a943bb7aa986d46e7a282aa1edc990da6
RUDNPRO <DbIM@mail.ru>
2020-12-07 11:27:07 +0300

    lesson 9 DZ ArrayInteger

--- src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
+++ src/ru/progwards/java1/lessons/bigints/BigAlgebra.java
@@ -32,12 +32,12 @@ public class BigAlgebra {
         return fibonacci(n -2).add(fibonacci(n - 1));
     }
 
-       public static void main(String[] args) {
+/*       public static void main(String[] args) {
         BigDecimal x = new BigDecimal("2");
         System.out.println(fastPow(x, 11));
         for (int i = 0; i<30;i++) {
             System.out.println(fibonacci(i));
         }
-    }
+    }*/
 }
 
