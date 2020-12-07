8864aa3ae2988f739e5829b57f6587f22aaa0346
RUDNPRO <DbIM@mail.ru>
2020-12-07 11:58:51 +0300

    lesson 9 DZ ArrayInteger

--- src/ru/progwards/java1/lessons/bigints/AbsInteger.java
+++ src/ru/progwards/java1/lessons/bigints/AbsInteger.java
@@ -1,107 +1,98 @@
 package ru.progwards.java1.lessons.bigints;
 
-//2.1 Реализовать класс AbsInteger - абстрактное целое число
-public abstract class AbsInteger {
-    public int num;
-
-    //2.3 У класса AbsInteger реализовать метод
-    // static AbsInteger add(AbsInteger num1, AbsInteger num2) -
-    // который вычисляет сумму num1 и num2.
-    // При этом надо учесть, что num1 и num2 могут быть разных типов.
-    // Для того, чтобы это реализовать продумать, какие дополнительные методы нужно сделать у AbsInteger и его потомков.
-
-    //Привести результат к наиболее подходящему типу, т.е. -128..127 это ByteInteger и т.д.
+public class AbsInteger {
+    protected int bytes;
     static AbsInteger add(AbsInteger num1, AbsInteger num2) {
-        return num1.num >= num2.num ? num1.addPlus(num2) : num2.addPlus(num1);
+        return num1.bytes >= num2.bytes ? num1.add2(num2) : num2.add2(num1);
     }
-    AbsInteger addPlus(AbsInteger num1) {
+    protected AbsInteger add2(AbsInteger num1) {
         return null;
     }
-
-    byte toByte() {
+    protected byte toByte() {
+        return 0;
+    }
+    protected short toShort() {
+        return 0;
+    }
+    protected int toInt() {
         return 0;
     }
 
-    short toShort() {
-        return 0;
+    public static void main(String[] args) {
+        AbsInteger num1 = new ByteInteger((byte)10);
+        AbsInteger num2 = new ShortInteger((short)1314);
+        System.out.println(AbsInteger.add(num1, num2));
     }
-
-    int toInt() {
-        return 0;
-    }
-
 }
 
-//2.2 Реализовать потомков ByteInteger, ShortInteger, IntInteger, хранящих значение целого числа соответствующего типа.
-// У каждого типа реализовать конструктор, принимающей значение числа соответствующего типа,
-// для ByteInteger - byte, ShortInteger - short и IntInteger - int.
-// Реализовать унаследованные публичные методы String toString(),
-// посредством которых возвращать приведенное к строке значение соответствующего классу типа.
 class ByteInteger extends AbsInteger {
     byte value;
     ByteInteger(byte value) {
         this.value = value;
+        bytes = 1;
     }
     @Override
     public String toString() {
         return Byte.toString(value);
     }
     @Override
-    public byte toByte() {
+    protected byte toByte() {
         return value;
     }
     @Override
-    public short toShort() {
+    protected short toShort() {
         return value;
     }
     @Override
-    public int toInt() {
+    protected int toInt() {
         return value;
     }
     @Override
-    AbsInteger addPlus(AbsInteger num1) {
+    protected AbsInteger add2(AbsInteger num1) {
         return new ByteInteger((byte) (value + num1.toByte()));
     }
-
 }
+
 class ShortInteger extends AbsInteger {
     short value;
     ShortInteger(short value) {
         this.value = value;
+        bytes = 2;
     }
     @Override
     public String toString() {
         return Short.toString(value);
     }
     @Override
-    public short toShort() {
+    protected short toShort() {
         return value;
     }
     @Override
-    public int toInt() {
+    protected int toInt() {
         return value;
     }
     @Override
-    AbsInteger addPlus(AbsInteger num1) {
+    protected AbsInteger add2(AbsInteger num1) {
         return new ShortInteger((short) (value + num1.toShort()));
     }
 }
+
 class IntInteger extends AbsInteger {
     int value;
     IntInteger(int value) {
         this.value = value;
+        bytes = 4;
     }
     @Override
     public String toString() {
         return Integer.toString(value);
     }
     @Override
-    public int toInt() {
+    protected int toInt() {
         return value;
     }
     @Override
-    AbsInteger addPlus(AbsInteger num1) {
+    protected AbsInteger add2(AbsInteger num1) {
         return new IntInteger((int) (value + num1.toInt()));
     }
 }
-
