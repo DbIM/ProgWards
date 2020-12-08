aaf121e8072df79d606e099e44e57d8a8ee9b672
RUDNPRO <DbIM@mail.ru>
2020-12-08 12:05:15 +0300

    lesson 10 LineCount

---
+++ src/ru/progwards/java1/lessons/io1/LineCount.java
@@ -0,0 +1,29 @@
+package ru.progwards.java1.lessons.io1;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class LineCount {
+    public static int calcEmpty(String fileName) throws IOException{
+        int quantity = 0;
+        FileReader reader = null;
+        try {
+            reader = new FileReader(fileName);
+            try {
+                Scanner scanner = new Scanner(reader);
+                while (scanner.hasNextLine()) {
+                    String strFromFile = scanner.nextLine();
+                    if (strFromFile.equals("")){
+                        quantity++;
+                    }
+                }
+            } finally {
+                reader.close();
+            }
+        } catch (IOException e) {
+            throw new IOException("-1");
+        }
+        return quantity;
+    }
+}
