83b9c7f1f30724dced957754286ca9ac5f311057
RUDNPRO <DbIM@mail.ru>
2020-12-09 20:25:35 +0300

    lesson 10 LineCount(checked) Coder CharFilter

--- src/ru/progwards/java1/lessons/io1/Coder.java
+++ src/ru/progwards/java1/lessons/io1/Coder.java
@@ -13,7 +13,53 @@ public class Coder {
     // соответствует символ code[(int)symbol] выходного файла.
     // В случае ошибок, в файл с именем logName вывести название ошибки
     // через метод класса Exception - getMessage()
-    public static void codeFile(String inFileName, String outFileName, char[] code, String logName) {
+    public static void codeFile(String inFileName, String outFileName, char[] code, String logName)
+    {
+        try
+        {
+            FileInputStream reader = new FileInputStream(inFileName);
+            FileOutputStream writer = new FileOutputStream(outFileName);
+
+            try
+            {
+                byte[] bytes = reader.readAllBytes();
+
+                for (int i = 0; i < bytes.length; i++)
+                    bytes[i] = (byte) code[bytes[i]];
+
+                writer.write(bytes);
+
+            } finally
+            {
+                writer.close();
+                reader.close();
+            }
+        } catch (IOException | ArrayIndexOutOfBoundsException e)
+        {
+            toLog(logName, e.getMessage());
+        }
+    }
+
+    public static void toLog(String logName, String msg)
+    {
+        System.out.println(msg);
+        try
+        {
+            FileWriter log = new FileWriter(logName, true);
+//
+            try
+            {
+                log.write(msg + "\n");
+            } finally
+            {
+                log.close();
+            }
+        } catch (IOException e)
+        {
+            System.out.println(e.getMessage());
+        }
+    }
+    /*{
         FileInputStream fileIn = null;
         FileOutputStream outFile = null;
         FileWriter logFile = null;
@@ -39,7 +85,7 @@ public class Coder {
         } catch (IOException e) {
             e.printStackTrace();
         }
-    }
+    }*/
 
     public static void main(String[] args) {
         String filename = "src\\ru\\progwards\\java1\\lessons\\io1\\file1111.txt";
