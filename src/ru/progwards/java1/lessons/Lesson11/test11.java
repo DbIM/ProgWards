704e155fbc925b604dbfa22a3020655704d2e0bc
RUDNPRO <DbIM@mail.ru>
2020-12-16 14:27:50 +0300

    lesson 11 Except DZ

--- src/ru/progwards/java1/lessons/Lesson11/test11.java
+++ src/ru/progwards/java1/lessons/Lesson11/test11.java
@@ -1,22 +1,67 @@
 package ru.progwards.java1.lessons.Lesson11;
 
-import java.io.IOException;
-
+import java.io.*;
+import java.util.Scanner;
 public class test11 {
 
     //правильный метод
+/*
     public void test(int n) throws IOException {
         try {
             doSomething(n);
-        }
-        catch (Throwable e){
+        } catch (Throwable e) {
             System.out.println(e.getMessage());
             throw new IOException();
-        }
-            finally {
+        } finally {
             System.out.println("finally executed");
         }
     }
+    */
+
+    public void scanLines() {
+        Scanner scanner = new Scanner(System.in);
+        while (scanner.hasNext()) {
+            String keyboard = scanner.nextLine();
+            if (keyboard.contains("Привет")) System.out.println("Здравствуйте!");
+            else if (keyboard.contains("как дела")) System.out.println("Хорошо");
+            else if (keyboard.contains("/stop")) break;
+            else System.out.println(keyboard);
+        }
+    }
+
+    static String invertWords(String sentence) {
+        String[] words = sentence.split(" ");
+        String backWords = "";
+        for (int i = words.length - 1; i >= 0; i--) {
+            backWords = backWords + words[i] + ".";
+        }
+        return backWords.substring(0, backWords.length() - 1);
+    }
+
+    //который читает файл filename и меняет в нем каждый 10-йбайт на символ *,
+// при этом конкатенируя оригинальный символ в строку ответа.
+    public static String setStars(String filename) throws IOException {
+        String finalString = "";
+        try (RandomAccessFile randomFile = new RandomAccessFile(filename, "rw")) {
+            long totalByte = randomFile.length();
+            for (int i = 9; i < totalByte; i += 10) {
+                randomFile.seek(i);
+                finalString = finalString + (char) randomFile.readUnsignedByte();
+                randomFile.seek(i);
+                randomFile.writeByte((int) '*');
+            }
+        } catch (IOException e) {
+            throw new IOException(e.getClass().getName());
+        }
+        return finalString;
+    }
+
+    public static void main(String[] args) throws IOException {
+        //System.out.println(invertWords("Буря мглою небо кроет"));
+        String filename = "src\\ru\\progwards\\java1\\lessons\\Lesson11\\test111.txt";
+        System.out.println(setStars(filename));
+    }
+
 }
 
 //Правильные ответы:
@@ -37,3 +82,23 @@ public class test11 {
 //doExceptions 2
 //Exception in finally
 //→ значение-параметра = 1
+
+//Правильные ответы:
+// Пакет java.io предоставляет возможности для работы с потоками, файлами и каталогами,
+// Класс File позволяет: получать и изменять основную информацию о файле или каталоге,
+// Класс File позволяет: создавать файлы, каталоги и цепочки каталогов,
+// Класс Scanner позволяет обрабатывать ввод с клавиатуры и из файлов
+
+//Правильные ответы:
+// RandomAccessFile позволяет перемещаться по файлу побайтно в разные стороны с указанием позиции,
+// RandomAccessFile позволяет читать, записывать (и перезаписывать) данные в текущую позицию,
+// RandomAccessFile умеет читать и записывать строки
+
+//Правильный ответ:
+// Записать что-то побайтно → void write…(...),
+// Переместить курсор к позиции → void seek(long pos),
+// Считывает и возвращает байт (беззнаковое значение) → int read(),
+// Прочитать массив байт в буфер bytes → int read(byte[] bytes),
+// Возвращает текущую позицию курсора → long getFilePointer(),
+// Прочитать строку → String readLine(),
+// Считывает требуемое количество байт и возвращает указанный тип → read…()
