f1a27da60ae1df9fddd809766e187a4cbbcc224f
RUDNPRO <DbIM@mail.ru>
2021-01-08 16:23:25 +0300

    DZ 12 Creator + Finder + Arrayiterator 08.01.21

--- src/ru/progwards/java1/lessons/collections/Creator.java
+++ src/ru/progwards/java1/lessons/collections/Creator.java
@@ -8,32 +8,34 @@ public class Creator {
     //создать коллекцию и заполнить последовательностью четных
     // возрастающих чисел начиная с 2,
     // количество элементов в коллекции n
-    public static Collection<Integer> fillEven(int n){
+    public static Collection<Integer> fillEven(int n) {
         Collection<Integer> result = new ArrayList();
         int x = 2;
         for (int i = n; i > 0; i--, x += 2) result.add(x);
         return result;
     }
-//создать коллекцию и заполнить последовательностью нечетных убывающих чисел,
+
+    //создать коллекцию и заполнить последовательностью нечетных убывающих чисел,
 // минимальное число в коллекции 1, количество элементов в коллекции n
-    public static Collection<Integer> fillOdd(int n){
+    public static Collection<Integer> fillOdd(int n) {
         Collection<Integer> result = new ArrayList();
-        int x =  n * 2 - 1;
+        int x = n * 2 - 1;
         for (int i = n; i > 0; i--, x -= 2) result.add(x);
         return result;
     }
-//создать коллекцию и заполнить ее тройками чисел.
+
+    //создать коллекцию и заполнить ее тройками чисел.
 // Каждая тройка создается по алгоритму: первое число тройки - индекс числа в коллекции,
 // второе - тот же индекс в квадрате,
 // третье - тот же индекс в кубе, количество элементов в коллекции n*3
-    public static Collection<Integer> fill3(int n){
-        Collection<Integer> result = new ArrayList();
-        int x2;
-        for (int i = 0; i < n * 3; i++) {
+    public static Collection<Integer> fill3(int n) {
+        Collection<Integer> result = new ArrayList<>();
+        int i2;
+        for (int i = 0; i < n; i++) {
             result.add(i);
-            x2 = i * i;
-            result.add(x2);
-            result.add(x2 * i);
+            i2 = i * i;
+            result.add(i2);
+            result.add(i2 * i);
         }
         return result;
     }
