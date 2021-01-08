dd68b06d42301b115c9c48cb0d9b02123492415a
RUDNPRO <DbIM@mail.ru>
2021-01-08 16:24:28 +0300

    DZ 12 Creator + Finder + Arrayiterator 08.01.21

---
+++ src/ru/progwards/java1/lessons/collections/MatrixIterator.java
@@ -0,0 +1,49 @@
+package ru.progwards.java1.lessons.collections;
+
+public class MatrixIterator<T> implements Iterator<T> {
+
+    private T[][] array;
+    int len;
+    int idx;
+    int nextIdx;
+    ArrayIterator<T> iterator;
+    ArrayIterator<T> nextIterator;
+
+    MatrixIterator(T[][] array) {
+        this.array = array;
+        len = array == null ? -1 : array.length;
+        idx = -1;
+        calcNextIterator();
+        iterator = nextIterator;
+        idx = nextIdx;
+    }
+
+    void calcNextIterator() {
+        int i = idx + 1;
+        while (i < len) {
+            if (array[i] != null && array[i].length > 0) {
+                nextIterator = new ArrayIterator<>(array[i]);
+                nextIdx = i;
+                return;
+            }
+            i++;
+        }
+        nextIterator = null;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return nextIterator != null;
+    }
+
+    @Override
+    public T next() {
+        if (!iterator.hasNext()) {
+            idx = nextIdx;
+            iterator = nextIterator;
+        }
+        T result = iterator.next();
+        if (!iterator.hasNext()) calcNextIterator();
+        return result;
+    }
+}
