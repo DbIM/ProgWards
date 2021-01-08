09d3ac65408166db11e652b47de57448a22adb7b
RUDNPRO <DbIM@mail.ru>
2021-01-08 16:51:22 +0300

    DZ 12 Creator + Finder + Iterators 08.01.21

--- src/ru/progwards/java1/lessons/collections/Finder.java
+++ src/ru/progwards/java1/lessons/collections/Finder.java
@@ -33,7 +33,7 @@ public class Finder {
     }
 
     public static void main(String[] args) {
-        System.out.println(findMinSumPair());
+     //   System.out.println(findMinSumPair());
     }
 
 
