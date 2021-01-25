fadb1689338e1aabcc2feacf14e170f129d53153
RUDNPRO <DbIM@mail.ru>
2021-01-25 22:21:48 +0300

    DZ 15 25/01/21

--- src/ru/progwards/java1/lessons/maps/FiboMapCache.java
+++ src/ru/progwards/java1/lessons/maps/FiboMapCache.java
@@ -4,131 +4,54 @@ import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
 
-import static java.lang.System.currentTimeMillis;
-
 public class FiboMapCache {
+    private Map<Integer, BigDecimal> fiboCache;
     private boolean cacheOn;
-
-    FiboMapCache(boolean cacheOn) {
+    public FiboMapCache(boolean cacheOn){
+        fiboCache = new HashMap<>();
         this.cacheOn = cacheOn;
-        clearCahe();
+    }
+    public void clearCahe(){
+        fiboCache.clear();
     }
 
-
-    public void clearCahe() {
-        if (cacheOn) {
-            if (fiboCache != null) fiboCache.clear();
-            fiboCache = new HashMap<Integer, BigDecimal>();
-            fiboCache.put(1, BigDecimal.ONE);
-            fiboCache.put(2, BigDecimal.ONE);
-            lastFiboCache_n = 2;
-            lastFibo_f1 = BigDecimal.ZERO;
-            lastFibo_f2 = BigDecimal.ONE;
-            lastFibo_n = 1;
-        }
+    public BigDecimal fiboNumber(int n){
+        BigDecimal result = BigDecimal.ZERO;
+        if (cacheOn)
+            if (fiboCache.containsKey(n))
+                result = fiboCache.get(n);
+            else {
+                result = fibonacci(n);
+                fiboCache.put(n, result);
+            }
+        else
+            result = fibonacci(n);
+        return result;
     }
-
-
-    public BigDecimal fiboNumber(int n) {
-        return cacheOn ? fiboCacheMap(n) : fiboNoCache(n);
+    //  рассчет числа Фибоначчи для n
+    static BigDecimal fibonacci(int n){
+        BigDecimal f_n_2 = BigDecimal.ZERO;
+        BigDecimal f_n_1 = BigDecimal.ONE;
+        BigDecimal f_n = BigDecimal.ONE;
+        for (int i = 2; i <= n; i++){
+            f_n = f_n_2.add(f_n_1);
+            f_n_2 = f_n_1;
+            f_n_1 = f_n;
+        }
+        return f_n;
     }
+    public static void test(){
+        FiboMapCache test1 = new FiboMapCache(false);
+        long start1 = System.currentTimeMillis();
+        for (int i=1; i<=1000; i++)
+            test1.fiboNumber(i);
+        System.out.println("fiboNumber cacheOn = false время выполнения = " + (System.currentTimeMillis() - start1));
 
-
-
-    Map<Integer, BigDecimal> fiboCache;
-    int lastFiboCache_n;
-
-    public BigDecimal fiboCacheMap(int n) {
-
-        if (fiboCache.containsKey(n)) return fiboCache.get(n);
-
-        BigDecimal f1 = fiboCache.get(lastFiboCache_n - 1);
-        BigDecimal f2 = fiboCache.get(lastFiboCache_n);
-        BigDecimal t;
-        int i = lastFiboCache_n;
-
-        while (i++ < n) {
-            t = f2;
-            f2 = f2.add(f1);
-            f1 = t;
-            fiboCache.put(i, f2);
-            if (i % 3 == 0)
-                try {
-                    Thread.sleep(1);
-                } catch (InterruptedException ex) {
-                }
-        }
-
-        lastFiboCache_n = n;
-        return f2;
-    }
-
-
-    int lastFibo_n = 1;
-    BigDecimal lastFibo_f1 = BigDecimal.ZERO;
-    BigDecimal lastFibo_f2 = BigDecimal.ONE;
-
-    public BigDecimal fiboCacheLast(int n) {
-
-        if (lastFibo_n == n) return lastFibo_f2;
-
-        BigDecimal f1;
-        BigDecimal f2;
-        BigDecimal t;
-        int i;
-
-        if (lastFibo_n < n) {
-            f1 = lastFibo_f1;
-            f2 = lastFibo_f2;
-            i = lastFibo_n;
-        } else {
-            f1 = BigDecimal.ZERO;
-            f2 = BigDecimal.ONE;
-            i = 1;
-        }
-
-        while (i++ < n) {
-            t = f2;
-            f2 = f2.add(f1);
-            f1 = t;
-        }
-
-        lastFibo_n = n;
-        lastFibo_f1 = f1;
-        lastFibo_f2 = f2;
-        return f2;
-    }
-
-
-    public BigDecimal fiboNoCache(int n) {
-        try { Thread.sleep(100);
-        } catch (InterruptedException ex) {
-        }
-        BigDecimal f1 = BigDecimal.ZERO;
-        BigDecimal f2 = BigDecimal.ONE;
-        BigDecimal t;
-        int i = 1;
-        while (i++ < n) {
-            t = f2;
-            f2 = f2.add(f1);
-            f1 = t;
-        }
-        return f2;
-    }
-
-    public static void test() {
-        long start;
-        FiboMapCache f;
-
-        start = currentTimeMillis();
-        f = new FiboMapCache(false);
-        for (int i = 1; i <= 1000; i++) f.fiboNumber(i);
-        System.out.println("fiboNumber cacheOn=" + false + " время выполнения " + (currentTimeMillis() - start));
-
-        start = currentTimeMillis();
-        f = new FiboMapCache(true);
-        for (int i = 1; i <= 1000; i++) f.fiboNumber(i);
-        System.out.println("fiboNumber cacheOn=" + true + " время выполнения " + (currentTimeMillis() - start));
+        FiboMapCache test2 = new FiboMapCache(true);
+        long start2 = System.currentTimeMillis();
+        for (int i=1; i<=1000; i++)
+            test2.fiboNumber(i);
+        System.out.println("fiboNumber cacheOn = true время выполнения = " + (System.currentTimeMillis() - start2));
 
     }
 }
