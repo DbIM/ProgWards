e84e72aaa887733b781fea1d3ae391461893eb4f
DbIM <DbIM@mail.ru>
2021-03-29 17:48:40 +0300

    GameStarted 28/03/21 beta dog

--- src/Game/ClassicRPGGame.java
+++ src/Game/ClassicRPGGame.java
@@ -8,7 +8,6 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
-
 public class ClassicRPGGame implements ActionListener {
     private static JFrame frame;
     private static JPanel panel;
@@ -21,17 +20,72 @@ public class ClassicRPGGame implements ActionListener {
 
         panel = new JPanel();
         frame.add(panel);
-
         panel.setLayout(null);
 
-        BufferedImage buttonIcon = ImageIO.read(new File("src/Game/img/bowbutton2.JPG"));
+        ActionListener bowButtonActionListener = new bowButtonActionListener();
+
+        BufferedImage buttonIcon = ImageIO.read(new File("src/Game/img/buttons/bowbutton2.JPG"));
         JButton button = new JButton(new ImageIcon(buttonIcon));
         button.setBorder(BorderFactory.createEmptyBorder());
         button.setFocusPainted(false);
         button.setContentAreaFilled(false);
         button.setBounds(740,230,97,82);
-        button.addActionListener(this);
+        button.addActionListener(bowButtonActionListener);
         panel.add(button);
+
+        BufferedImage upleftbuttonicon = ImageIO.read(new File("src/Game/img/buttons/upleft.JPG"));
+        JButton upleftbutton = new JButton(new ImageIcon(upleftbuttonicon));
+        upleftbutton.setBorder(BorderFactory.createEmptyBorder());
+        upleftbutton.setFocusPainted(false);
+        upleftbutton.setContentAreaFilled(false);
+        upleftbutton.setBounds(750,470,79,65);
+        upleftbutton.addActionListener(this);
+        panel.add(upleftbutton);
+
+        BufferedImage upbuttonicon = ImageIO.read(new File("src/Game/img/buttons/up.JPG"));
+        JButton upbutton = new JButton(new ImageIcon(upbuttonicon));
+        upbutton.setBorder(BorderFactory.createEmptyBorder());
+        upbutton.setFocusPainted(false);
+        upbutton.setContentAreaFilled(false);
+        upbutton.setBounds(825,470,88,71);
+        upbutton.addActionListener(this);
+        panel.add(upbutton);
+
+        BufferedImage uprightbuttonicon = ImageIO.read(new File("src/Game/img/buttons/upright.JPG"));
+        JButton uprightbutton = new JButton(new ImageIcon(uprightbuttonicon));
+        uprightbutton.setBorder(BorderFactory.createEmptyBorder());
+        uprightbutton.setFocusPainted(false);
+        uprightbutton.setContentAreaFilled(false);
+        uprightbutton.setBounds(910,470,82,70);
+        uprightbutton.addActionListener(this);
+        panel.add(uprightbutton);
+
+        BufferedImage leftbuttonicon = ImageIO.read(new File("src/Game/img/buttons/left.JPG"));
+        JButton leftbutton = new JButton(new ImageIcon(leftbuttonicon));
+        leftbutton.setBorder(BorderFactory.createEmptyBorder());
+        leftbutton.setFocusPainted(false);
+        leftbutton.setContentAreaFilled(false);
+        leftbutton.setBounds(746,538,83,67);
+        leftbutton.addActionListener(this);
+        panel.add(leftbutton);
+
+        BufferedImage downbuttonicon = ImageIO.read(new File("src/Game/img/buttons/down.JPG"));
+        JButton downbutton = new JButton(new ImageIcon(downbuttonicon));
+        downbutton.setBorder(BorderFactory.createEmptyBorder());
+        downbutton.setFocusPainted(false);
+        downbutton.setContentAreaFilled(false);
+        downbutton.setBounds(828,538,82,68);
+        downbutton.addActionListener(this);
+        panel.add(downbutton);
+
+        BufferedImage rightbuttonicon = ImageIO.read(new File("src/Game/img/buttons/right.JPG"));
+        JButton rightbutton = new JButton(new ImageIcon(rightbuttonicon));
+        rightbutton.setBorder(BorderFactory.createEmptyBorder());
+        rightbutton.setFocusPainted(false);
+        rightbutton.setContentAreaFilled(false);
+        rightbutton.setBounds(910,538,82,68);
+        rightbutton.addActionListener(this);
+        panel.add(rightbutton);
 
         BufferedImage backgroundImage = ImageIO.read(new File("src/Game/img/Newcastle.png"));
         JLabel backgroundLabel = new JLabel(new ImageIcon(backgroundImage));
@@ -41,8 +95,10 @@ public class ClassicRPGGame implements ActionListener {
         frame.setVisible(true);
     }
 
-    public static void main(String[] args) throws IOException {
-        new ClassicRPGGame();
+    public class bowButtonActionListener implements ActionListener {
+        public void actionPerformed(ActionEvent e) {
+            frame.dispose();
+        }
     }
 
     @Override
