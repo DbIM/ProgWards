27b46220d50ccf2ed2d3ca9806d8a7b417181df2
DbIM <DbIM@mail.ru>
2021-06-16 14:55:12 +0300

    Generics final task

--- src/ru/progwards/java2/lessons/generics/ArraySort.java
+++ src/ru/progwards/java2/lessons/generics/ArraySort.java
@@ -3,11 +3,11 @@ package ru.progwards.java2.lessons.generics;
 public class ArraySort {
     public static <T extends Comparable<T>> void sort(T[] args) {
         for(int i = 0; i < args.length-1; i++) {
-            for(int j = i+1; j < args.length; j++) {
-                if(args[i].compareTo(args[j]) > 0) {
+            for(int i2 = i+1; i2 < args.length; i2++) {
+                if(args[i].compareTo(args[i2]) > 0) {
                     T temp = args[i];
-                    args[i] = args[j];
-                    args[j] = temp;
+                    args[i] = args[i2];
+                    args[i2] = temp;
                 }
             }
         }
